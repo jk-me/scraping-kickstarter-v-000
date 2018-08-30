7202526a74fbc0ed27f092a55a7b335417ea64ac
Jenny K <jkam17@gmail.com>
2018-08-30 21:29:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,11 +10,16 @@ def create_project_hash
   
   kickstarter.css("li.project.grid_4").each {|p| 
     title= p.css("h2.bbcard_name strong a").text 
-    projects[title.to_sym]={}}
+    projects[title.to_sym]={
+      :image_link => p.css("div.project-thumbnail a img").attribute("src").value,
+      :description => p.css("p.bbcard_blurb").text,
+      :location => p.css("ul.project-meta span.location-name").text,
+      :percent_funded => p.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }}
     
   
   #binding.pry
-  
+  projects
 end
 
 
